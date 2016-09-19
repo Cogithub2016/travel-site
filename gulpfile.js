8fb359c4e4bbf46431fb6a4dc521eac9534163df
Cogithub2016 <judas.liscariote.007@gmail.com>
2016-09-19 21:22:14 +0200

    Completed large hero block and intro content block

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+/*gulp.task('default',function(){
+	console.log("Hooray - you created a Gulp task.");
+});
+gulp.task('html',function(){
+	console.log("Imagine something useful being done to your HTML here.");
+});*/
+/*gulp.task('styles',function(){
+	console.log("Imagine Sass or PostCSS tasks running here.");
+});*/
+
+
+
