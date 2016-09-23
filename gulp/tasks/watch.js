3b73091f12ceafe94b35e1a246d6d9b97509d3c2
Cogithub2016 <judas.liscariote.007@gmail.com>
2016-09-23 20:52:32 +0200

    Completed automatic sprites generation

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,23 +1,27 @@
-var gulp=require('gulp');
-var watch=require('gulp-watch'),
-browserSync=require('browser-sync').create();
-gulp.task('watch',function(){
-	
-	browserSync.init({
-		notify:false,
-		server:{
-			baseDir:"app"
-		}
-	});
-	watch('./app/index.html',function(){
-		/*gulp.start('html');*/
-		browserSync.reload();
-	});
-	watch('./app/assets/styles/**/*.css',function(){
-		gulp.start('cssInject');
-	});
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
 });
-gulp.task('cssInject',['styles'],function(){
-	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(browserSync.stream());
-})
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
